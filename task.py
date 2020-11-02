c88049e74423cb769035ef908f906febc6ff7cde
repl.it user <replituser@example.com>
2020-11-02 15:19:05 +0000

    File added

--- task.py
+++ task.py
@@ -3,7 +3,7 @@
 #Write a code in python that simulates the tilt correction of the plane (angle between plane wings and earth). 
 #The program should print out current orientation, and applied tilt correction.
 # (Tilt is "Roll" in this diagram https://www.novatel.com/assets/Web-Phase-2-2012/Solution-Pages/AttitudePlane.png)
-#The program should run in infinite loop, until user breaks the loop. 
+#The program should run in infinite loop, until user breaks the loop.
 #Assume that plane orientation in every new simulation step is changing with random angle with gaussian distribution (the planes is experiencing "turbuence"). 
 # Hint: "random.gauss(0, 2*rate_of_correction)"
 #With every simulation step the orentation should be corrected, correction should be applied and printed out.
@@ -22,3 +22,48 @@
 #Delete these comments before commit!
 #
 #Good luck.
+import random
+
+class Airplane:
+  def __init__(self, roll, pitch, yaw):
+    self.expected_roll = roll
+    self.expected_pitch = pitch
+    self.expected_yaw = yaw
+    self.roll = roll
+    self.pitch = pitch
+    self.yaw = yaw
+
+  def update_parameters (self, d_roll, d_pitch, d_yaw):
+    self.roll += d_roll
+    self.pitch += d_pitch
+    self.yaw += d_yaw
+
+  def correct_parameters (self, rate_of_correction):
+    self.roll -= rate_of_correction*(self.expected_roll-self.roll)
+    self.pitch -= rate_of_correction*(self.expected_pitch-self.pitch)
+    self.yaw -= rate_of_correction*(self.expected_yaw-self.yaw)
+    
+
+airplane = Airplane(0, 15, 0)
+
+while True:
+  roll_turbulance = random.random()
+  pitch_turbulance = random.random()
+  yaw_turbulance = random.random()
+
+  airplane.update_parameters(roll_turbulance, pitch_turbulance, yaw_turbulance)
+  print("Current parameters:\nroll: {} (expected: {})\npitch: {} (expected: {})\nyaw: {} (expected: {})\n".format(
+    airplane.roll, airplane.expected_roll, airplane.pitch, airplane.expected_pitch, airplane.yaw, airplane.expected_yaw
+  ))
+  airplane.correct_parameters(0.5)
+  print ("Correction done\n")
+
+  if (airplane.pitch < 0):
+    print ("Plane crashed")
+    break
+  
+    
+
+
+    
+
